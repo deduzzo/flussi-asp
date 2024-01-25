6e592417769a54a1d95a24c5ea61fd75a61edeba
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2024-01-25 10:30:38 +0100

    inserimento ok

--- views/adi/index.php
+++ views/adi/index.php
@@ -15,6 +15,12 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <?php $form = ActiveForm::begin() ?>
 
+<style>
+    .dataTable-table td, .dataTable-table th {
+        font-size: 14px;
+    }
+</style>
+
 <div class="alert alert-primary" role="alert">
     <?php
     echo Html::a('Nuova PIC', ['adi/nuova'], ['class' => 'btn btn-primary']);
@@ -28,25 +34,54 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= GridView::widget([
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
-            'options' => ['class' => 'table-responsive'],
-            'tableOptions' => ['class' => 'table table-striped table-bordered fs-6'], // Aggiungi qui le classi di Bootstrap
+            'pager' => [
+                'class' => 'yii\bootstrap5\LinkPager',
+                'firstPageLabel' => 'PRIMA',
+                'lastPageLabel' => 'ULTIMA',
+                'nextPageLabel' => '>>',
+                'prevPageLabel' => '<<',
+                'linkOptions' => ['class' => 'page-link'],
+            ],
+            'options' => [
+                'tag' => 'div',
+                'class' => 'dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns',
+                'id' => 'datatable',
+            ],
+            'tableOptions' => [
+                'class' => 'table table-striped dataTable-table',
+                'id' => 'table1',
+            ],
             'columns' => [
                 'id',
+                [
+                    'attribute' => 'distretto',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        return Html::tag('strong', Html::encode($model->distretto));
+                    },
+                ],
                 'cf',
-                'distretto',
+                'cognome',
+                'nome',
                 'data_pic:date',
                 'inizio:date',
                 'fine:date',
                 'cartella_aster',
-                'cognome',
-                'nome',
-                'dittaScelta.denominazione',
+                [
+                    'attribute' => 'dittaScelta.denominazione',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        return $model->dittaScelta && $model->dittaScelta->denominazione
+                            ? Html::tag('strong', Html::encode($model->dittaScelta->denominazione))
+                            : 'N/A';
+                    },
+                ],
                 [
                     // notificato of type badge success or no
                     'attribute' => 'data_ora_invio',
                     'format' => 'raw',
                     'value' => function ($model) {
-                        return $model->data_ora_invio !== null ? ('<span class="badge bg-success">OK</span>') : ('<span class="badge bg-danger">PAI non ancora inviato</span>');
+                        return $model->data_ora_invio !== null ? ('<span class="badge bg-success">OK</span>') : ('<span class="badge bg-danger">Non inviato</span>');
                     },
                     'label' => 'Notifica'
                 ],
