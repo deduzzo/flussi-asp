f04158251cc8ebbc4776d2e999cf51a3075e1736
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2024-02-12 11:21:51 +0100

    inserimento ok

--- config/mail_example.php
+++ config/mail_example.php
@@ -1,18 +1,14 @@
 <?php
+$user = $params['adminEmail'];
+$password = '';
+$host = 'posta.asp.messina.it';
 
+$user = urlencode($user);
+$password = urlencode($password); // Codifica la password
 return [
     'class' => \yii\symfonymailer\Mailer::class,
     'transport' => [
-        'scheme' => 'tls',
-        'host' => '',
-        'username' => '',
-        'password' => '',
-        'port' => 465,
-        'dsn' => 'native://default',
+        "dsn" => "smtp://$user:$password@$host?encryption=tls&auth_mode=login&verify_peer=false",
     ],
-    'viewPath' => '@common/mail',
-    // send all mails to a file by default. You have to set
-    // 'useFileTransport' to false and configure transport
-    // for the mailer to send real emails.
     'useFileTransport' => false,
 ];
